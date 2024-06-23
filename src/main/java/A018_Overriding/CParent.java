923953d75eb9303410ba05937d0af85702668850
ErkanCeviz <erkanceviz1985@gmail.com>
2024-06-24 01:25:22 +0200

    24Haz

---
+++ src/main/java/A018_Overriding/CParent.java
@@ -0,0 +1,16 @@
+package A018_Overriding;
+
+public class CParent {
+
+    public void method1(){
+        System.out.println("ffffff");
+
+    }
+    private void met1(){
+
+        System.out.println("met1");
+
+
+    }
+
+}
---
+++ src/main/java/A018_Overriding/DChild.java
@@ -0,0 +1,37 @@
+package A018_Overriding;
+
+public class DChild extends CParent{
+
+    public void method1(){
+        System.out.println("ddddddddddddd");
+    }
+
+    void met1(){
+
+        System.out.println("child met1");
+    }
+
+
+    public static void main(String[] args) {
+
+
+        CParent ob1=new DChild();
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
